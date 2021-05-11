489b56d32285f8fe21cc60e5ba213170a3dcca9c
Harvey Mei <harvey.mei@msn.com>
2021-05-11 16:42:12 +0800

    Commit Message

--- highs_lows.py
+++ highs_lows.py
@@ -10,3 +10,13 @@
 数据可视化之CSV文件操作
 """
 
+import csv
+
+filename = 'sitka_weather_07-2014.csv'
+with open(filename) as f:  # 打开文件
+    reader = csv.reader(f)  # 读取文件
+    header_row = next(reader)  # 调用方法读取下一行
+    # print(header_row)  # 打印读取到的内容
+    # 打印文件头及其位置
+    for index, column_header in enumerate(header_row):  # 遍历并获取每个元素的索引及其值
+        print(index, column_header)  # 打印索引及对应值
