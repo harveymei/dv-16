71d86c5673b11375d5719e1e93b20cce139e11df
Harvey Mei <harvey.mei@msn.com>
2021-05-12 15:52:58 +0800

    Commit Message

--- highs_lows2.py
+++ highs_lows2.py
@@ -11,7 +11,9 @@ from matplotlib import pyplot as plt
 # 处理日期数据
 from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+# filename = 'sitka_weather_07-2014.csv'
+# 更长时间周期的数据文件
+filename = 'sitka_weather_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -20,7 +22,7 @@ with open(filename) as f:
     # 提取指定列的数据
     # highs = []  # 定义最高温度空列表
     # 定义日志列表和最高温度列表
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:  # 遍历列表，阅读器对象从其停留的地方继续往下读取CSV文件，每次都自动返回当前所处位置的下一行。
         # 遍历日期写入列表
         # https://docs.python.org/3/library/datetime.html#datetime.date.strftime
@@ -31,6 +33,9 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     # print(highs)  # 打印列表
 
 # 绘制图形
@@ -38,9 +43,13 @@ fig = plt.figure(dpi=128, figsize=(10, 6))
 # plt.plot(highs, c='red')  # 红色显示最高气温
 # 添加日期数据
 plt.plot(dates, highs, c='red')
+# 绘制最低温度
+plt.plot(dates, lows, c='blue')
 
 # 设置图形格式
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+# plt.title("Daily high temperatures, July 2014", fontsize=24)
+# 标题由月度改为年
+plt.title("Daily high and low temperatures, 2014", fontsize=24)
 plt.xlabel("", fontsize=16)  # 暂未包含日期数据，x轴标签为空
 # 绘制斜的日期标签，以免它们彼此重叠
 fig.autofmt_xdate()
