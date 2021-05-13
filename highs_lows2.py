c9828319d0d2e0bf87566851813bda42e7466662
Harvey Mei <harvey.mei@msn.com>
2021-05-13 09:44:22 +0800

    Commit Message

--- highs_lows2.py
+++ highs_lows2.py
@@ -13,7 +13,7 @@ from datetime import datetime
 
 # filename = 'sitka_weather_07-2014.csv'
 # 更长时间周期的数据文件
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -24,17 +24,22 @@ with open(filename) as f:
     # 定义日志列表和最高温度列表
     dates, highs, lows = [], [], []
     for row in reader:  # 遍历列表，阅读器对象从其停留的地方继续往下读取CSV文件，每次都自动返回当前所处位置的下一行。
-        # 遍历日期写入列表
-        # https://docs.python.org/3/library/datetime.html#datetime.date.strftime
-        current_date = datetime.strptime(row[0], '%Y-%m-%d')
-        dates.append(current_date)
-        # highs.append(row[1])  # 将列表中的第一列值依次写入highs列表
-        # 将字符串转换为数值，以供matplotlib使用
-        high = int(row[1])
-        highs.append(high)
+        # 从CSV文件中读取值时执行错误检查代码，对分析数据集时可能出现的异常进行处理
+        try:
+            # 遍历日期写入列表
+            # https://docs.python.org/3/library/datetime.html#datetime.date.strftime
+            current_date = datetime.strptime(row[0], '%Y-%m-%d')
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            # highs.append(row[1])  # 将列表中的第一列值依次写入highs列表
+            # 将字符串转换为数值，以供matplotlib使用
 
-        low = int(row[3])
-        lows.append(low)
+            highs.append(high)
+            lows.append(low)
 
     # print(highs)  # 打印列表
 
@@ -56,7 +61,7 @@ plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 # 设置图形格式
 # plt.title("Daily high temperatures, July 2014", fontsize=24)
 # 标题由月度改为年
-plt.title("Daily high and low temperatures, 2014", fontsize=24)
+plt.title("Daily high and low temperatures, 2014\nDeath Valley, CA", fontsize=20)
 plt.xlabel("", fontsize=16)  # 暂未包含日期数据，x轴标签为空
 # 绘制斜的日期标签，以免它们彼此重叠
 fig.autofmt_xdate()
